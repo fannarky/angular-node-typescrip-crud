7e632e4b59eeb57b2377c71a0c83910a63119b59
Phelipe Soares Simeao <pssimeao@stefanini.com>
2019-05-17 15:00:29 -0300

    mais configurações para funcionamento do server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-console.log('It Works')
+import express, { Application } from 'express';
+
+class Server{
+
+    public app: Application
+
+    constructor(){
+        this.app = express()
+        this.config()
+        this.routes()
+    }
+
+    config(): void{
+        this.app.set('port', process.env.PORT || 8080)
+    }
+
+    routes() :void{
+
+    }
+
+    start(): void{
+        this.app.listen(this.app.get('port'), () => {
+            console.log(`Server on port ${this.app.get('port')}, Cuck!!`)
+        })
+    }
+
+}
+
+const server = new Server()
+server.start()
