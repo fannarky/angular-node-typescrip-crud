05448a6ef04b4b227dfb746d1e2611fd297e91d2
Phelipe Soares Simeao <pssimeao@stefanini.com>
2019-05-17 15:38:54 -0300

    banco

---
+++ database/database.sql
@@ -0,0 +1,12 @@
+create database ng_games_db;
+
+use ng_games_db;
+
+create table game
+(
+    id int(11) not null auto_increment primary key,
+    title
+    description
+    image   
+    created_at
+)
